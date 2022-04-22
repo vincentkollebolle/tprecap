f311ca5013e43e217c66a713b183561206fd7ebc
pample <bonnier.vincentpro@gmail.com>
2022-04-22 15:09:43 +0200

    mise en place menu

--- vue/layout.php
+++ vue/layout.php
@@ -19,8 +19,30 @@
     </script>
   </head>
   <body>
-  
+        
     <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <a class="navbar-brand" href="#">Navbar</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="/frontController.php">Tous les articles</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#">ajouter un article</a>
+                  </li>
+                </ul>
+               
+              </div>
+            </nav>
+          </div>
+        </div>
         <div class="row">
             <div class="col-12">
                 <?=$content?>
